78d3556502a9670bec53a632199a477d6491d4bc
Maxim Safronov <dev.safronov.maxim@gmail.com>
2022-07-15 13:34:16 +0300

    Mobile version implementation

--- src/main/java/edu/safronov/controllers/RootController.java
+++ src/main/java/edu/safronov/controllers/RootController.java
@@ -3,10 +3,20 @@ package edu.safronov.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
 @Controller
 public class RootController {
     @GetMapping("/")
-    public String showRootView() {
+    public String showRootView(HttpServletRequest request) {
+        Optional<String> mobileHeader = Optional.ofNullable(request.getHeader("user-agent"));
+        if (mobileHeader.isPresent()) {
+            if (mobileHeader.get().contains("Android") || mobileHeader.get().contains("iPhone")) {
+                System.out.println("Mobile client");
+                return "index-mobile"; //TODO: необходимо разработать шаблон для смартфонов
+            }
+        }
         return "index";
     }
 }
