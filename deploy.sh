c553a86734397b54d0464a09c5c0673b5d6e6307
Maxim Safronov <dev.safronov.maxim@gmail.com>
2022-08-01 22:35:18 +0300

    Deploy script fixes

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,6 @@ git fetch
 git pull
 
 docker stop call-request-service
+docker rm call-request-service
 docker build -t call-request-service:1.0-SNAPSHOT .
 docker run --name call-request-service -d -p 8080:8080 call-request-service:1.0-SNAPSHOT
